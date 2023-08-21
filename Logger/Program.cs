617bc2ad2de903d68d94adf14472bfcb8ed17f74
acer <acer@ASUR>
2023-08-21 09:03:34 +0530

    Add project files.

---
+++ Logger/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+
+static class Logger
+{
+    public static void LogMessage(string message)
+    {
+        Console.WriteLine($"[LOG] {DateTime.Now:yyyy-MM-dd HH:mm:ss} - {message}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Logger.LogMessage("Starting the application...");
+
+        // Simulating some application logic
+        for (int i = 1; i <= 5; i++)
+        {
+            Logger.LogMessage($"Performing task {i}");
+        }
+
+        Logger.LogMessage("Application finished.");
+        Console.ReadLine();
+    }
+}
